faa76d4a67acc0e1ee7a4fa6d213508a5aef60bb
Elamanchik00 <elamansultanaliev04@gmail.com>
2022-12-16 18:30:10 +0600

    created model Type

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Articles
+from .models import Articles, Type
 
 admin.site.register(Articles)
-# Register your models here.
+admin.site.register(Type)
